52794182e9734c7e3d87149bde00fa61803a0485
Rafael Amarantes <rafael@amarantes.com.br>
2018-09-04 17:36:27 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
@@ -6,10 +6,26 @@ import { createStore, applyMiddleware } from 'redux';
 import App from './components/app';
 import reducers from './reducers';
 
+import {BrowserRouter, Route  } from 'react-router-dom';
+
 const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+class Hello extends Component{
+  render(){return <div> Hello </div>}
+}
+class GoodBye extends Component{
+  render(){return <div>Goodbye </div>}
+}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        Teste
+      <Route path="/hello" component={Hello}/>
+      <Route path="/GoodBye" component={GoodBye}/>
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
+
