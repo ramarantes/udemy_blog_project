c9d4a633026654a03b57c39ad608317d34edea20
Rafael Amarantes <rafael@amarantes.com.br>
2018-09-11 18:12:28 -0300

    using redux-form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,14 +1,41 @@
 import React, {Component} from 'react';
+import {Field, reduxForm} from 'redux-form';
 
 class PostNew extends Component {
+
+    renderField(field){
+        return (
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input 
+                    className="form-control"
+                    type="text"
+                    placeholder={field.label}
+                    {...field.input}
+                />
+            </div>
+        );
+    }
+    
     render(){
         console.log('postNew');
         return( 
-            <div>
-                PostNew
-            </div>
+            <form>
+                    <Field name="title" label="Title"  component={this.renderField} />
+                    <Field name="Categories" label="categories"  component={this.renderField} />
+                    <Field name="content" label="Content"  component={this.renderField} />
+
+                </form>
         );
     }
 }
 
-export default PostNew;
+function validate(values){
+    console.log(values);
+
+}
+
+export default reduxForm({
+    validate,
+    form:'PostsNewForm'
+})(PostNew);
